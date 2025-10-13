291c9cc291453ae2cd6103b1775432569ecc91fa
loofkid <loofkid@gmail.com>
2025-10-12 22:09:55 -0400

    Change credential name from 'snmp' to 'Snmp'

--- nodes/Snmp/SnmpTrapTrigger.node.ts
+++ nodes/Snmp/SnmpTrapTrigger.node.ts
@@ -24,7 +24,7 @@ export class SnmpTrapTrigger implements INodeType {
 		credentials: [
 			{
 				// eslint-disable-next-line n8n-nodes-base/node-class-description-credentials-name-unsuffixed
-				name: 'snmp',
+				name: 'Snmp',
 				required: false,
 			},
 		],
